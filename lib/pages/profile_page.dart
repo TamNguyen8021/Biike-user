8d3319a41b18eaeb15a1f3b2768c2a130f0e3794
Do Huu Phat <dhphat@outlook.com>
2021-07-23 16:38:32 +0700

    phatdh_create_ui

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,89 @@
+import 'package:bikes_user/utils/custom_colors.dart';
+import 'package:bikes_user/utils/custom_strings.dart';
+import 'package:bikes_user/widgets/appbars/custom_appbar.dart';
+import 'package:bikes_user/widgets/appbars/bottom_tabbar.dart';
+import 'package:bikes_user/widgets/pages/activity.dart';
+import 'package:bikes_user/widgets/pages/customer_home_full.dart';
+import 'package:bikes_user/widgets/pages/driver_home.dart';
+import 'package:flutter/material.dart';
+
+/// The home screen template for both ke-er and biker
+class ProfilePage extends StatelessWidget {
+  final String role;
+  const ProfilePage({Key? key, required this.role}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget homePage = CustomerHomeFull();
+    Widget activityPage = Activity(
+      role: role,
+    );
+    String modeButtonText = CustomStrings.kCustomerMode;
+    Color modeButtonForegroundColor = CustomColors.kBlue;
+    Color modeButtonBackgroundColor = CustomColors.kLightGray;
+    if (role != 'Customer') {
+      homePage = DriverHome();
+      modeButtonText = CustomStrings.kBikerMode;
+      modeButtonBackgroundColor = CustomColors.kOrange;
+      modeButtonForegroundColor = CustomColors.kLightGray;
+    }
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: CustomAppBar(
+          appBar: AppBar(),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 5.0),
+            child: Row(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/logo-blue',
+                  height: 25.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: SizedBox(
+                    height: 20,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                        child: Text(
+                          modeButtonText,
+                          style: TextStyle(
+                              color: modeButtonForegroundColor, fontSize: 10),
+                        ),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              modeButtonBackgroundColor),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)))),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actionWidgets: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.account_circle_rounded,
+                    color: Colors.white,
+                  )),
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children: <Widget>[homePage, activityPage],
+        ),
+        bottomNavigationBar: BottomTabBar(),
+      ),
+    );
+  }
+}
