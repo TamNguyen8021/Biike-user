5f189b4434949de761548e66d846907ae6bf12aa
UyenNLP1902 <nguyenlephuonguyen.1902@gmail.com>
2021-07-24 19:36:23 +0700

    - Add missing icon

--- lib/pages/biker_feedback_page.dart
+++ lib/pages/biker_feedback_page.dart
@@ -1,9 +1,12 @@
 import 'package:bikes_user/utils/custom_colors.dart';
 import 'package:bikes_user/utils/custom_strings.dart';
 import 'package:bikes_user/widgets/buttons/send_feedback_button.dart';
+import 'package:bikes_user/widgets/half_oval_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BikerFeedbackPage extends StatelessWidget{
   const BikerFeedbackPage({Key? key}) : super(key: key);
@@ -16,105 +19,117 @@ class BikerFeedbackPage extends StatelessWidget{
         resizeToAvoidBottomInset: false,
         body: Container(
           alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.all(22.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Flexible(
-                          child: Text(
-                            //TODO: icon
-                              "Chỗ này là icon mặt cười"
-                          ),
-                        ),
-                      ]
-                  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 200,
+                child: Stack(
+                  // alignment: Alignment.center,
+                  children: <Widget>[
+                    CustomPaint(
+                      painter: HalfOvalPainter(),
+                      child: Container(height: 150),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: SvgPicture.asset(
+                        'assets/images/happy-face.svg',
+                        height: 120,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Flexible(
-                          child: Text(
-                            CustomStrings.tripSuccess,
-                            overflow: TextOverflow.clip,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: CustomColors.kBlue, fontSize: 25, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Flexible(
-                          child: Text(
-                            CustomStrings.rateReminder,
-                            overflow: TextOverflow.clip,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: CustomColors.kDarkGray, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        RatingBar.builder(
-                          //initialRating: 3,
-                            minRating: 1,
-                            allowHalfRating: false,
-                            itemCount: 5,
-                            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                            onRatingUpdate: (rating) {
-                              print(rating);
-                            },
-                            itemBuilder: (context, _) => Icon(
-                              Icons.star,
-                              color: Colors.amber,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(22.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                CustomStrings.tripSuccess,
+                                overflow: TextOverflow.clip,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: CustomColors.kBlue, fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
                             ),
-                        ),
-                      ]
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Card(
-                    color: CustomColors.kLightGray,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextField(
-                        maxLines: 3,
-                        maxLength: 250,
-                        inputFormatters: [LengthLimitingTextInputFormatter(250)],
-                        decoration: InputDecoration(
-                          counter: Offstage(),
+                          ]
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                CustomStrings.rateReminder,
+                                overflow: TextOverflow.clip,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: CustomColors.kDarkGray, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RatingBar.builder(
+                              //initialRating: 3,
+                              minRating: 1,
+                              allowHalfRating: false,
+                              itemCount: 5,
+                              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                              onRatingUpdate: (rating) {
+                                print(rating);
+                              },
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Card(
+                        color: CustomColors.kLightGray,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextField(
+                            maxLines: 3,
+                            maxLength: 250,
+                            inputFormatters: [LengthLimitingTextInputFormatter(250)],
+                            decoration: InputDecoration(
+                              counter: Offstage(),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[SendFeedbackButton()]
-                  ),
-                ),
-              ]
-            ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[SendFeedbackButton()]
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ]
           ),
         ),
       ),
---
+++ lib/pages/keer_feedback_page.dart
@@ -0,0 +1,160 @@
+import 'package:bikes_user/utils/custom_colors.dart';
+import 'package:bikes_user/utils/custom_strings.dart';
+import 'package:bikes_user/widgets/buttons/send_feedback_button.dart';
+import 'package:bikes_user/widgets/half_oval_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class KeerFeedbackPage extends StatelessWidget{
+  const KeerFeedbackPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  child: Stack(
+                    // alignment: Alignment.center,
+                    children: <Widget>[
+                      CustomPaint(
+                        painter: HalfOvalPainter(),
+                        child: Container(height: 150),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SvgPicture.asset(
+                          'assets/images/happy-face.svg',
+                          height: 120,
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(22.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Flexible(
+                                child: Text(
+                                  CustomStrings.tripSuccess,
+                                  overflow: TextOverflow.clip,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: CustomColors.kBlue, fontSize: 25, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ]
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Flexible(
+                                child: Text(
+                                  CustomStrings.rateReminder,
+                                  overflow: TextOverflow.clip,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: CustomColors.kDarkGray, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ]
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              RatingBar.builder(
+                                //initialRating: 3,
+                                minRating: 1,
+                                allowHalfRating: false,
+                                itemCount: 5,
+                                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                },
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                              ),
+                            ]
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Card(
+                          color: CustomColors.kLightGray,
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              maxLines: 3,
+                              maxLength: 250,
+                              inputFormatters: [LengthLimitingTextInputFormatter(250)],
+                              decoration: InputDecoration(
+                                counter: Offstage(),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[SendFeedbackButton()]
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Flexible(
+                                child: Text(
+                                  CustomStrings.buyReminder,
+                                  overflow: TextOverflow.clip,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: CustomColors.kDarkGray, fontSize: 12, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ]
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SvgPicture.asset(
+                          'assets/images/coffee-cup.svg',
+                          height: 120,
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
