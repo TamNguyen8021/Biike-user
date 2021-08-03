6bde5ff9c3be483492bdb0b8d1832cb2628bcc58
Tam Nguyen <tamntt8021@gmail.com>
2021-08-03 20:38:59 +0700

    [BIIKE-46] Finish screen '007-input-phone'
    Signed-off-by: Tam Nguyen <tamntt8021@gmail.com>

--- lib/pages/input_phone_page.dart
+++ lib/pages/input_phone_page.dart
@@ -15,52 +15,69 @@ class InputPhonePage extends StatefulWidget {
 class _InputPhonePageState extends State<InputPhonePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: CustomColors.kBlue,
-        body: Padding(
-          padding: const EdgeInsets.only(top: 100.0, left: 22.0, right: 22.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(CustomStrings.kInputPhoneNumber,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                  )),
-              IntlPhoneField(
-                keyboardType: TextInputType.phone,
-                initialCountryCode: 'VN',
-                initialValue: '34 866 9124',
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.only(top: 20.0),
-                    errorText: CustomStrings.kErrorInputPhoneNumber,
-                    errorStyle: TextStyle(color: Colors.white, fontSize: 14),
-                    counterText: ""),
-                dropdownDecoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                showDropdownIcon: false,
-                inputFormatters: [MaskedInputFormatter('00 000 0000')],
+    return Scaffold(
+      backgroundColor: CustomColors.kBlue,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/line-map.png'),
+                fit: BoxFit.fitWidth,
+                colorFilter: ColorFilter.mode(
+                    CustomColors.kLightGray.withOpacity(0.5),
+                    BlendMode.dstIn))),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 100.0, left: 22.0, right: 22.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(CustomStrings.kInputPhoneNumber,
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      )),
+                  IntlPhoneField(
+                    keyboardType: TextInputType.phone,
+                    initialCountryCode: 'VN',
+                    initialValue: '34 866 9124',
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(top: 20.0),
+                        errorText: CustomStrings.kErrorInputPhoneNumber,
+                        errorStyle:
+                            TextStyle(color: Colors.white, fontSize: 14),
+                        counterText: "",
+                        focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    CustomColors.kLightGray.withOpacity(0.8))),
+                        errorBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color:
+                                    CustomColors.kLightGray.withOpacity(0.8)))),
+                    dropdownDecoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    showDropdownIcon: false,
+                    inputFormatters: [MaskedInputFormatter('00 000 0000')],
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        floatingActionButton: Stack(children: [
-          FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.white,
-            child: Icon(
-              Icons.arrow_forward_ios,
-              color: CustomColors.kBlue,
             ),
-          ),
-          // Image(image: AssetImage(''),)
-        ]),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.arrow_forward_ios,
+          color: CustomColors.kBlue,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
