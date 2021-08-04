dd7cd5a2caa54e0207ddac3bfca64b216207cdec
Tam Nguyen <tamntt8021@gmail.com>
2021-08-04 14:08:18 +0700

    [BIIKE-51] Update screen '010-choose-role'
    Signed-off-by: Tam Nguyen <tamntt8021@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -32,48 +32,55 @@ class Biike extends StatelessWidget {
       // Remove debug banner when running app.
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          // Apply a combination of colors based on the provided one to entire app.
-          primarySwatch: CustomColors.kBlue,
-          scaffoldBackgroundColor: Colors.white,
-          fontFamily: 'SVN Product Sans',
-          appBarTheme: AppBarTheme(
-              backwardsCompatibility: false,
-              elevation: 0.0,
-              backgroundColor: CustomColors.kBlue,
-              titleTextStyle: TextStyle(
-                  fontSize: 13,
-                  color: Colors.white,
-                  letterSpacing: 0.5,
-                  fontFamily: 'SVN Product Sans')),
-          textTheme: TextTheme(
-              bodyText1: TextStyle(fontSize: 12, color: CustomColors.kDarkGray),
-              bodyText2: TextStyle(color: CustomColors.kDarkGray),
-              button: TextStyle(fontSize: 10, color: Colors.white)),
-          tabBarTheme: TabBarTheme(
-            indicatorSize: TabBarIndicatorSize.label,
-          ),
-          iconTheme: IconThemeData(color: CustomColors.kBlue, size: 15),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(CustomColors.kBlue),
-                  textStyle: MaterialStateProperty.all<TextStyle>(
-                      TextStyle(color: Colors.white, fontSize: 12)),
-                  elevation: MaterialStateProperty.all<double>(2.0))),
-          inputDecorationTheme: InputDecorationTheme(
-              contentPadding: const EdgeInsets.only(top: 12.0),
-              labelStyle: TextStyle(fontWeight: FontWeight.normal))),
+        // Apply a combination of colors based on the provided one to entire app.
+        primarySwatch: CustomColors.kBlue,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'SVN Product Sans',
+        appBarTheme: AppBarTheme(
+            backwardsCompatibility: false,
+            elevation: 0.0,
+            backgroundColor: CustomColors.kBlue,
+            titleTextStyle: TextStyle(
+                fontSize: 13,
+                color: Colors.white,
+                letterSpacing: 0.5,
+                fontFamily: 'SVN Product Sans')),
+        textTheme: TextTheme(
+            bodyText1: TextStyle(fontSize: 12, color: CustomColors.kDarkGray),
+            bodyText2: TextStyle(color: CustomColors.kDarkGray),
+            button: TextStyle(fontSize: 10, color: Colors.white),
+            headline1: TextStyle(fontSize: 25, color: Colors.white),
+            headline2: TextStyle(
+                fontSize: 18,
+                color: CustomColors.kBlue,
+                fontWeight: FontWeight.bold)),
+        tabBarTheme: TabBarTheme(
+          indicatorSize: TabBarIndicatorSize.label,
+        ),
+        iconTheme: IconThemeData(color: CustomColors.kBlue, size: 15),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(CustomColors.kBlue),
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                    TextStyle(color: Colors.white, fontSize: 12)),
+                elevation: MaterialStateProperty.all<double>(2.0))),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.only(top: 12.0),
+          labelStyle: TextStyle(fontWeight: FontWeight.normal),
+        ),
+      ),
       initialRoute: '/inputName',
       routes: {
         '/open': (BuildContext ctx) => OpenPage(),
         '/welcome': (BuildContext ctx) => WelcomePage(),
         '/inputPhone': (BuildContext ctx) => InputPhonePage(),
         '/inputName': (BuildContext ctx) => InputNamePage(),
+        '/chooseRole': (BuildContext ctx) => ChooseRolePage(),
         '/history': (BuildContext ctx) => TripHistoryPage(),
         '/driverHome': (BuildContext ctx) => HomePage(
               role: 'Driver',
             ),
-        '/chooseRole': (BuildContext ctx) => ChooseRolePage(),
         '/customerHome': (BuildContext ctx) => HomePage(
               role: 'Customer',
             ),
