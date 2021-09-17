2a7e26fd271124cf4d14310bc52af643f52690d0
UyenNLP1902 <53332541+UyenNLP1902@users.noreply.github.com>
2021-09-17 12:36:22 +0700

    Feedback done (#11)
    * - Gộp feedback của keer và biker vào làm một để khi sửa code chỉ cần sửa một nơi
    
    * Remove unused import
    
    * - Add filter bad words
    - Create page for errors msg
    
    * -Create a custom bad word filter
    
    * - Rename wrong name

---
+++ lib/utils/BadWordsFilter/bad_word_list/bad_word_vi.dart
@@ -0,0 +1,13 @@
+List<String> badWordsListVi = [
+  'địt',
+  'đm',
+  'đụ',
+  'vl',
+  'đít',
+  'cặc',
+  'cứt',
+  'lồn',
+  'buồi',
+  'đbrr',
+  'đbgr'
+];
---
+++ lib/utils/custom_error_strings.dart
@@ -0,0 +1,9 @@
+class FeedbackError {
+  static const String kNotRated = "Please rate your trip experience";
+  static const String kIncludeBadWords = "Please don\'t use bad words";
+  static const String kSendFailed = "Send feedback failed!";
+}
+
+class SearchTripError {
+  //TODO
+}
