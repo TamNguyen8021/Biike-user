6a92ba0f6141eb026b6ebdef1d854868f2493cc0
VanLNT <vanlntse140065@fpt.edu.vn>
2021-09-30 22:02:41 +0700

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:bikes_user/app/data/enums/role_enum.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -10,7 +11,10 @@ import 'app/routes/app_pages.dart';
 import 'app/routes/app_routes.dart';
 
 /// Runs the application.
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(Biike());
 }
 
