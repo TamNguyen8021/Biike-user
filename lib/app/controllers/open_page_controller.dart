139e9889787f7f67a8788585465b3ab10c7c2ce8
Tam Nguyen <tamntt8021@gmail.com>
2021-10-17 20:27:50 +0700

    Add device log file
    Signed-off-by: Tam Nguyen <tamntt8021@gmail.com>

--- lib/app/controllers/open_page_controller.dart
+++ lib/app/controllers/open_page_controller.dart
@@ -1,4 +1,3 @@
-
 import 'package:bikes_user/app/routes/app_routes.dart';
 import 'package:bikes_user/injectable/injectable.dart';
 import 'package:bikes_user/services/firebase_services.dart';
@@ -7,8 +6,10 @@ import 'package:get/get.dart';
 
 class OpenPageController extends GetxController {
   static OpenPageController get to => Get.find<OpenPageController>();
+
   final AppPref _appPref = getIt<AppPref>();
   final FirebaseServices _firebaseServices = getIt<FirebaseServices>();
+
   Future<void> navigateToWelcomeScreen() async {
     await Future.delayed(Duration(seconds: 3)).then((value) {
       if (_appPref.initOpenApp) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:bikes_user/app/common/functions/local_app_data.dart';
 import 'package:bikes_user/app/data/enums/role_enum.dart';
@@ -6,6 +7,7 @@ import 'package:bikes_user/app/ui/theme/app_theme.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_logs/flutter_logs.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -53,26 +55,108 @@ void main() {
 }
 
 /// This widget is the root of your application.
-class Biike extends StatelessWidget {
-  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+//ignore: must_be_immutable
+class Biike extends StatefulWidget {
   static final LocalAppData localAppData = LocalAppData();
   static Rx<Role> role = Role.none.obs;
   static Rx<int> userId = (-1).obs;
   static Rx<String> token = ''.obs;
 
+  const Biike({Key? key}) : super(key: key);
+
+  @override
+  State<Biike> createState() => _BiikeState();
+}
+
+class _BiikeState extends State<Biike> {
+  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+  RxMap _deviceData = {}.obs;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+    loadDataFromLocal();
+    FlutterLogs.logToFile(
+        logFileName: 'Device',
+        overwrite: true,
+        logMessage: _deviceData.entries.toString());
+  }
+
   /// Load token, role, and userId from local
   ///
   /// Author: TamNTT
   void loadDataFromLocal() async {
-    token.value = await localAppData.token;
-    userId.value = await localAppData.userId;
-    String roleString = await localAppData.role;
-    role.value = role.value.getRoleEnum(roleString);
+    Biike.token.value = await Biike.localAppData.token;
+    Biike.userId.value = await Biike.localAppData.userId;
+    String roleString = await Biike.localAppData.role;
+    Biike.role.value = Biike.role.value.getRoleEnum(roleString);
+  }
+
+  Future<void> initPlatformState() async {
+    try {
+      if (Platform.isAndroid) {
+        _deviceData.value = _readAndroidBuildData(await deviceInfo.androidInfo);
+      } else {
+        _deviceData.value = _readIosDeviceInfo(await deviceInfo.iosInfo);
+      }
+    } on PlatformException {
+      _deviceData.putIfAbsent('Error', () => 'Failed to get platform version.');
+    }
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'androidId': build.androidId,
+      'systemFeatures': build.systemFeatures,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
   }
 
   @override
   Widget build(BuildContext context) {
-    loadDataFromLocal();
     return ScreenUtilInit(
       designSize: Size(360, 780),
       builder: () => GetMaterialApp(
