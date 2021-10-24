ee7792bd7fb5e91b77ebd628c9d52bb64ea62abd
VanLNT <vanlntse140065@fpt.edu.vn>
2021-10-24 18:49:20 +0700

    implement block and unblock user

--- lib/app/ui/android/pages/ban_list/ban_list_page.dart
+++ lib/app/ui/android/pages/ban_list/ban_list_page.dart
@@ -1,18 +1,17 @@
 import 'package:bikes_user/app/common/values/custom_strings.dart';
+import 'package:bikes_user/app/controllers/ban_list_controller.dart';
 import 'package:bikes_user/app/ui/android/widgets/appbars/custom_appbar.dart';
-import 'package:bikes_user/app/ui/android/widgets/cards/ban_list_card.dart';
 import 'package:bikes_user/app/ui/android/widgets/lists/list_ban.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 
 /// The 'block_list' screen
-class BanListPage extends StatelessWidget {
+class BanListPage extends HookWidget {
   const BanListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final banListController = Get.find<BanListController>();
-
     return Scaffold(
       appBar: CustomAppBar(
         isVisible: true,
@@ -26,16 +25,26 @@ class BanListPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(children: <Widget>[
           Container(
-              height: MediaQuery.of(context).size.height,
-              padding: const EdgeInsets.all(22.0),
-              child: ListBan(
-                banList: [
-                  BanListCard(name: 'Võ Thị Kim Thuỳ'),
-                  BanListCard(name: 'Võ Thị Kim Thuỳ'),
-                  BanListCard(name: 'Võ Thị Kim Thuỳ'),
-                ],
-                itemPadding: 10.0,
-              )),
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.all(22.0),
+            child: GetBuilder<BanListController>(
+              builder: (controller) {
+                if (controller.data == null) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if(controller.data!.isEmpty){
+                  return Text('chua co ai xui xeo bi unblock');
+                }
+                return ListBan(
+                  itemPadding: 10.0,
+                  data: controller.data!,
+                  onTapItem: (blackListItem) {
+                    controller.unBlock(blackListItem);
+                  },
+                );
+              },
+            ),
+          ),
         ]),
       ),
     );
--- lib/network/config_network.dart
+++ lib/network/config_network.dart
@@ -4,4 +4,6 @@ class EndPoint {
   static const String signup = '/api/biike/v1/users';
 
   static const String userVerify = '/api/biike/v1/users';
+
+  static const String blackList = '/api/biike/v1/intimacies';
 }
