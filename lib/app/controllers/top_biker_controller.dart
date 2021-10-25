f4c9c704a45c3f425504269fd55a0d7855a6e286
Đỗ Hữu Phát <phatdh@gmail.com>
2021-10-25 14:31:05 +0700

    topbiker : get data

--- lib/app/controllers/top_biker_controller.dart
+++ lib/app/controllers/top_biker_controller.dart
@@ -1,4 +1,36 @@
+import 'package:bikes_user/app/data/models/user.dart';
+import 'package:bikes_user/app/data/providers/top_biker_provider.dart';
+import 'package:bikes_user/app/ui/android/widgets/cards/top_biker_card.dart';
 import 'package:get/get.dart';
 
 /// Manage states for [TopBikerPage]
-class TopBikerController extends GetxController {}
+class TopBikerController extends GetxController {
+  RxList topBiker = [].obs;
+
+  Future<void> getTopBiker() async {
+    topBiker.clear();
+
+    dynamic response = await TopBikerProvider().getTopBiker();
+    if (response != null) {
+      try {
+        var index = 0;
+        for (var biker in response) {
+          User user = User.fromJson(biker);
+
+          topBiker.add(
+              TopBikerCard(
+                  index: index,
+                  avatarUrl: user.avatar,
+                  name: user.userFullname,
+                  point: user.totalPoint
+              )
+          );
+
+          index ++;
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+}
